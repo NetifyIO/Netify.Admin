a9b8e36f7a9809b80d540419bb79a3a5584f7c35
Brandon Clapp <Clapp09@gmail.com>
2017-12-17 20:07:24 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,20 @@
 ﻿
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var debug = require('gulp-debug');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+
+gulp.task('deploy', function() {
+  var remotePath = '/httpdocs/admin.netify.io/';
+  var conn = ftp.create({
+    host: 'netify.io',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
+  });
+  gulp.src(['publish/**/*'])
+    .pipe(debug())
+    .pipe(conn.dest(remotePath));
+});
